421b8cdf9d54cbd3d2b9853eaac9ec35babaa564
swyxio <shawnthe1@gmail.com>
2023-11-09 10:02:52 -0800

    init

---
+++ main.py
@@ -0,0 +1,40 @@
+from openai import OpenAI
+import base64
+
+client = OpenAI()
+# Function to encode the image
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
+
+
+
+# Path to your image
+image_path = "swyx.jpeg"
+
+# Getting the base64 string
+base64_image = encode_image(image_path)
+
+response = client.chat.completions.create(
+    model="gpt-4-vision-preview",
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                {"type": "text", "text": "What’s in this image?"},
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": f"data:image/jpeg;base64,{base64_image}"
+                    },
+                },
+            ],
+        }
+    ],
+    max_tokens=300,
+)
+
+print(response.choices[0])
+
+
+

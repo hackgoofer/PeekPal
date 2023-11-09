6d31629b87716305efdece7cd96478a0dfe66fa1
Sasha <sashashengyt@gmail.com>
2023-11-09 10:51:56 -0800

    no loop, but will loop soon

--- main.py
+++ main.py
@@ -1,40 +1,117 @@
 from openai import OpenAI
 import base64
+from dotenv import load_dotenv
+
+load_dotenv()
 
 client = OpenAI()
+
+
 # Function to encode the image
 def encode_image(image_path):
     with open(image_path, "rb") as image_file:
-        return base64.b64encode(image_file.read()).decode('utf-8')
+        return base64.b64encode(image_file.read()).decode("utf-8")
 
 
-
-# Path to your image
-image_path = "swyx.jpeg"
-
-# Getting the base64 string
-base64_image = encode_image(image_path)
+messages = [
+    {
+        "role": "user",
+        "content": [
+            {
+                "type": "text",
+                # "text": "According to the screenshot, what is the next step so that I can listen to the same song at the same time with a friend?",
+                "text": "describe the screenshot",
+            },
+            {
+                "type": "image_url",
+                "image_url": {
+                    "url": f"data:image/png;base64,{encode_image('screenshots/spotify.png')}"
+                },
+            },
+        ],
+    }
+]
 
 response = client.chat.completions.create(
     model="gpt-4-vision-preview",
-    messages=[
-        {
-            "role": "user",
-            "content": [
-                {"type": "text", "text": "What’s in this image?"},
-                {
-                    "type": "image_url",
-                    "image_url": {
-                        "url": f"data:image/jpeg;base64,{base64_image}"
-                    },
-                },
-            ],
-        }
-    ],
+    messages=messages,
     max_tokens=300,
 )
 
-print(response.choices[0])
+reply = response.choices[0]
+print("1111111")
+print(reply)
+
+messages.extend(
+    [
+        {
+            "role": "assistant",
+            "content": [
+                {
+                    "type": "text",
+                    "text": reply.message.content,
+                },
+            ],
+        },
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": "According to the screenshot, what is the next step so that I can listen to the same song at the same time with a friend?",
+                },
+            ],
+        },
+    ]
+)
+
+response = client.chat.completions.create(
+    model="gpt-4-vision-preview",
+    messages=messages,
+    max_tokens=300,
+)
+
+reply = response.choices[0]
+print("222222")
+print(reply)
 
 
+messages.extend(
+    [
+        {
+            "role": "assistant",
+            "content": [
+                {
+                    "type": "text",
+                    "text": reply.message.content,
+                },
+            ],
+        },
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": f"data:image/png;base64,{encode_image('screenshots/spotify2.png')}"
+                    },
+                },
+                {
+                    "type": "text",
+                    "text": "I got to this step, now what?",
+                },
+            ],
+        },
+    ]
+)
 
+response = client.chat.completions.create(
+    model="gpt-4-vision-preview",
+    messages=messages,
+    max_tokens=300,
+)
+
+reply = response.choices[0]
+print("33333")
+
+print(reply)

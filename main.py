5945e6151715bbb446485358e11113b402134b7e
Sasha <sashashengyt@gmail.com>
2023-11-09 12:22:47 -0800

    loop

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from openai import OpenAI
 import base64
 from dotenv import load_dotenv
+import time
+import os
+import glob
+import datetime
 
 load_dotenv()
 
@@ -12,6 +16,17 @@ def encode_image(image_path):
     with open(image_path, "rb") as image_file:
         return base64.b64encode(image_file.read()).decode("utf-8")
 
+
+def write_items_to_file(items, filename="what you should do.txt"):
+    with open(filename, "w") as file:
+        # Write items in reverse order
+        for item in reversed(items):
+            file.write(f"{item}\n")
+
+
+goal = "listen to the same song at the same time with a friend"
+with open("what i'm doing.txt", "r") as file:
+    goal = file.readline().strip()
 
 messages = [
     {
@@ -31,6 +46,7 @@ messages = [
         ],
     }
 ]
+replies = []
 
 response = client.chat.completions.create(
     model="gpt-4-vision-preview",
@@ -38,9 +54,9 @@ response = client.chat.completions.create(
     max_tokens=300,
 )
 
-reply = response.choices[0]
-print("1111111")
+reply = response.choices[0].message.content
 print(reply)
+replies.append(reply)
 
 messages.extend(
     [
@@ -58,7 +74,7 @@ messages.extend(
             "content": [
                 {
                     "type": "text",
-                    "text": "According to the screenshot, what is the next step so that I can listen to the same song at the same time with a friend?",
+                    "text": f"According to the screenshot, what is the next step so to achieve the goal: {goal}",
                 },
             ],
         },
@@ -71,47 +87,60 @@ response = client.chat.completions.create(
     max_tokens=300,
 )
 
-reply = response.choices[0]
-print("222222")
+reply = response.choices[0].message.content
 print(reply)
+replies.append(reply)
 
+while True:
+    # Get all screenshot files
+    screenshot_files = glob.glob("screenshots/screenshot_*.png")
+    screenshot_files = [screenshot.split(".")[0] for screenshot in screenshot_files]
+    # Parse the timestamp from the filename and sort the files by timestamp
+    sorted_screenshot_files = sorted(
+        screenshot_files,
+        key=lambda x: datetime.datetime.strptime(x.split("_")[1], "%Y-%m-%d-%H-%M-%S"),
+        reverse=True,
+    )
+    # Get the second latest screenshot
+    screenshot_to_parse = sorted_screenshot_files[1]
+    print(f"parsing screenshot: {screenshot_to_parse}.png")
+    encoded_image = encode_image(f"{screenshot_to_parse}.png")
 
-messages.extend(
-    [
-        {
-            "role": "assistant",
-            "content": [
-                {
-                    "type": "text",
-                    "text": reply.message.content,
-                },
-            ],
-        },
-        {
-            "role": "user",
-            "content": [
-                {
-                    "type": "image_url",
-                    "image_url": {
-                        "url": f"data:image/png;base64,{encode_image('screenshots/spotify2.png')}"
+    messages.extend(
+        [
+            {
+                "role": "assistant",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": reply,
                     },
-                },
-                {
-                    "type": "text",
-                    "text": "I got to this step, now what?",
-                },
-            ],
-        },
-    ]
-)
+                ],
+            },
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "image_url",
+                        "image_url": {"url": f"data:image/png;base64,{encoded_image}"},
+                    },
+                    {
+                        "type": "text",
+                        "text": "I got to this step, now what?",
+                    },
+                ],
+            },
+        ]
+    )
 
-response = client.chat.completions.create(
-    model="gpt-4-vision-preview",
-    messages=messages,
-    max_tokens=300,
-)
+    response = client.chat.completions.create(
+        model="gpt-4-vision-preview",
+        messages=messages,
+        max_tokens=300,
+    )
 
-reply = response.choices[0]
-print("33333")
-
-print(reply)
+    reply = response.choices[0].message.content
+    print(f"GPT replies: {reply}")
+    replies.append(reply)
+    write_items_to_file(replies)
+    time.sleep(2)
--- peek.py
+++ peek.py
@@ -13,16 +13,16 @@ __PIL_TUPLE_VERSION = tuple(int(x) for x in PIL.__version__.split("."))
 pyscreeze.PIL__version__ = __PIL_TUPLE_VERSION
 
 # Set the interval between screenshots in seconds
-interval = 5  # e.g., 5 seconds
+interval = 1  # e.g., 5 seconds
 
 # Set the number of screenshots to take
-number_of_screenshots = 10
+number_of_screenshots = 200
 
 
 # Function to take a screenshot
 def take_screenshot():
     # Get the current time in a readable format
-    timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
     # Set the filename with the current timestamp
     filename = f"screenshots/screenshot_{timestamp}.png"
     # Take a screenshot
